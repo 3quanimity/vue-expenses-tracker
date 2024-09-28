15bc3901a12c1d7e0ba9bc8881f307a5227fd512
Haythem <sinnner6@gmail.com>
2024-09-28 23:53:25 +0100

    add eslint + dynamically display a list of transactions, using the options api

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
     IncomeExpenses,
     TransactionList,
     AddTransaction,
-  }
+  },
 }
 </script>
 
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,19 +1,41 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 299.97 },
+        { id: 3, text: "Book", amount: -39.97 },
+        { id: 4, text: "Business", amount: 599.97 },
+      ],
+    };
+  },
 
+  methods: {
+    formatAmount(value) {
+      if (value >= 0) {
+        return "$" + value;
+      } else {
+        return "-$" + Math.abs(value);
+      }
+    },
+  },
+};
 </script>
 
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Paycheck <span>$800</span><button class="delete-btn">x</button>
+    <li
+      v-for="{ id, text, amount } in transactions"
+      :class="amount < 0 ? 'minus' : 'plus'"
+      :key="id"
+    >
+      {{ text }}
+      <span>{{ formatAmount(amount) }}</span>
+      <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
