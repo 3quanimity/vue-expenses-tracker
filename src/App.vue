1ac44258b38fc3cb4baa6aa49ef434ffb6145d42
Haythem <sinnner6@gmail.com>
2024-09-28 23:56:45 +0100

    resolve conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script>
-import Header from '@/components/Header.vue'
+import AppHeader from "@/components/AppHeader.vue";
 import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransaction from "@/components/AddTransaction.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Header,
+    AppHeader,
     Balance,
     IncomeExpenses,
     TransactionList,
     AddTransaction,
   },
-}
+};
 </script>
 
 <template>
-  <Header />
+  <AppHeader />
   <div class="container">
     <Balance />
     <IncomeExpenses />
